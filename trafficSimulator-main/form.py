a0e28e548234ab9f8554ad7a0cccbca4409787d5
agungfazrulhaq <agungfazrulhaq95@gmail.com>
2022-10-05 22:11:49 +0700

    Finished for second meeting with prof

--- trafficSimulator-main/form.py
+++ trafficSimulator-main/form.py
@@ -4,16 +4,24 @@ import os
 
 st.title("Traffic Simulator")
 
-with st.form("my_form"):
-    st.write("Parameters")
-    types = ["Intersection", "Roundabout"]
-    select_type = st.selectbox("Select road type", types)
-    slider_val = st.slider("Vehicle Density",0,120)
+
+st.write("Parameters")
+types = ["Intersection", "Roundabout"]
+select_type = st.selectbox("Select road type", types)
+# intersection_btn = st.button('Intersection')
+# roundabout_btn = st.button('Roundabout')
+slider_val = st.slider("Vehicle Density",0,120)
+
+if select_type == 'Intersection' :
     duration = st.number_input("Traffic lights duration", 10, 60)
 
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        st.write("density : ", slider_val, "Type :", select_type, "Traffic lights Duration :", duration)
-        command_ = "python twoway_intersection.py " + str(slider_val) + " " + str(duration)
-        os.system(command_)
+# Every form must have a submit button.
+submitted = st.button("Submit")
+if submitted:
+    st.write("Type : " + select_type)
+    if select_type=="Intersection" :
+        command_ = "python simulation_1.py "+ str(select_type) + " " + str(slider_val) + " " + str(duration)
+    else :
+        
+        command_ = "python simulation_1.py "+ str(select_type) + " " + str(slider_val)
+    os.system(command_)
